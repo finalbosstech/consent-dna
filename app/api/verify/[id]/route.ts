abd26cb1995e8ec81bdb2dc8bf610ac093466e58
805-ai <abman21111@gmail.com>
2025-08-25 21:40:28 -0700

    Update route.ts

--- app/api/verify/[id]/route.ts
+++ app/api/verify/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../../lib/db';
+import prisma from '../../../../ib/db';
 export const runtime = 'nodejs';
 
 type Params = { params: { id: string } };
